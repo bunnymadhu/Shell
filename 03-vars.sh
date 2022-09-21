6807715bf72126fcc623b92d5c64066a361e0357
madhu <madhusudhan002.mr@gmail.com>
2022-09-21 19:12:51 +0530

    Added to comments for shell scripting in 03-vars.sh file

--- 03-vars.sh
+++ 03-vars.sh
@@ -6,5 +6,14 @@ COURSE=DevOps
 # Access a Variable
 echo Course Name = $COURSE
 
-DATE= 2021-09-20
+#DATE= 2021-09-20
+DATE=$(date +%F)
 echo Good Morning, Today date is $DATE
+
+NO_OF_USERS=$(who | wc -l)    #Word Count=Wc     11 25 171   -l(11)=lines  25=words 171 charecters
+echo Number of Users Logged IN  in system = $NO_OF_USERS
+
+ADD=$((2+3+4+5+))
+
+BIG=$((10*3+2/4+6-5+8*9))
+echo BIG=$BIG
