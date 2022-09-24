4a725437a80a233555c173596ff1595d7fd0a883
madhu <madhusudhan002.mr@gmail.com>
2022-09-24 12:05:21 +0530

    Added to comments for shell scripting in 04-read.sh file

---
+++ 05-special-vars-input.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+## To get Script Name
+
+echo $0
+
+##  First argument to the script
+echo $1
+
+## Second argument to the script
+echi $2
+
+## Like wise we can go till $n
+
+## All the arguments parsed
+
+echo $*
+echo $@
+
+## Number of arguments
+
+echo $#
+
