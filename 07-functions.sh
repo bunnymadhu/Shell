2eacbea7eb4a55065590e6f5f954474263af7e01
madhu <madhusudhan002.mr@gmail.com>
2022-09-25 12:14:44 +0530

    Added to comments for shell scripting in 07-functions.sh file

--- 07-functions.sh
+++ 07-functions.sh
@@ -6,9 +6,14 @@ SAMPLE() {
    echo Welcome to SAMPLE function
    echo Value of a = $a
    b=20
+   echo first argument = $1
+   ## return 5
 }
 
 ## Access your Function
 a=10
-SAMPLE
+SAMPLE xyz
+SAMPLE $1
 echo Value of b = $b
+## echo Exit status of SAMPLE Function = $?
+## $? is nothing but the exit status  of the previous excuted command
